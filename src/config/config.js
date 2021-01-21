1b1df91ef890c73b12d0eca98a165e40e47211ad
Jesus Garcia <jelegaho@gmail.com>
2021-01-21 08:35:57 -0500

    add google-maps api

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const config = {
   app: {
     port: process.env.PORT || 3000,
+    api_key: process.env.API_KEY,
   }
 }
 
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,38 +1,8 @@
-const map = L.map('map');
-const tileURL = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
-let marker = {}
-const form = document.querySelector("#form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-})
+let map;
 
-
-// enable location to browser
-map.locate({ enableHighAccuracy: true});
-
-// event location found
-map.on('locationfound', locationOn)
-
-//event click on map
-map.on('click', onMapClick)
-// set tile to map
-L.tileLayer(tileURL).addTo(map);
-
-function locationOn(e){
-  const coords = [e.latlng.lat, e.latlng.lng];
-  map.setView(coords, 14)
-  L.marker(coords).addTo(map)
-    .bindPopup('¡You are here!')
-    .openPopup();
-}
-
-function onMapClick(e){
-  const coords = [e.latlng.lat, e.latlng.lng];
-
-  if(marker != undefined) {
-    map.removeLayer(marker);
-  }
-  marker = L.marker(coords).addTo(map)
-            .bindPopup('¡You select this place!')
-            .openPopup();
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 4.609, lng: -74.081},
+    zoom: 10,
+  });
 }
